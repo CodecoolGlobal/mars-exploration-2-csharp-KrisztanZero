bf31328c4e25f753b78a89a9153dedc175ffe27c
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-03-07 10:31:02 +0100

    implement IAnalyzer to LackOfResourceAnalyzer.cs

---
+++ Codecool.MarsExploration.MapExplorer/Simulation/OutcomeAnalyzer/LackOfResourceAnalyzer.cs
@@ -0,0 +1,15 @@
+﻿using Codecool.MarsExploration.MapExplorer.Exploration;
+using Codecool.MarsExploration.MapExplorer.Simulation.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Simulation.OutcomeAnalyzer;
+
+public class LackOfResourcesAnalyzer : IAnalyzer
+{
+    public ExplorationOutcome Outcome => ExplorationOutcome.Error;
+
+    public bool Analyze(SimulationContext context)
+    {
+        var exploredRatio = (double)context.Map.Count(ElementType.Empty) / context.Map.Size;
+        return exploredRatio <= 0.05; // 95% of the map has been explored
+    }
+}
