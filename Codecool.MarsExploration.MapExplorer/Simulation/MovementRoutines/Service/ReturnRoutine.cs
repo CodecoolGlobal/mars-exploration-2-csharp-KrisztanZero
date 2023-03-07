42f6d819ab508630dbb1713fe6ddd2bf9025a76f
Gergely Papp <gregpappdev.gmail.com>
2023-03-07 22:24:40 +0100

    implements Return routine of IMovementRoutine interface

---
+++ Codecool.MarsExploration.MapExplorer/Simulation/MovementRoutines/Service/ReturnRoutine.cs
@@ -0,0 +1,12 @@
+﻿using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Simulation.MovementRoutines.Service;
+
+public class ReturnRoutine : IMovementRoutine
+{
+    public Coordinate NextStep(Coordinate currentPosition, Map map)
+    {
+        throw new NotImplementedException();
+    }
+}
