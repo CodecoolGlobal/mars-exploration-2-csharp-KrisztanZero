1fcdb7004b5cc5eb674cdb3f8a6b5e2e2eb415a4
Gergely Papp <gregpappdev.gmail.com>
2023-03-07 22:52:58 +0100

    sets basic return route for rover in ReturnRoutine.cs

--- Codecool.MarsExploration.MapExplorer/Simulation/MovementRoutines/Service/ReturnRoutine.cs
+++ Codecool.MarsExploration.MapExplorer/Simulation/MovementRoutines/Service/ReturnRoutine.cs
@@ -8,6 +8,24 @@ public class ReturnRoutine : IMovementRoutine
 {
     public Coordinate NextStep(SimulationContext context)
     {
-        throw new NotImplementedException();
+        var spaceShipLocation = context.SpaceShipLocation;
+
+        var y = Math.Abs(spaceShipLocation.Y - context.Rover.currentPosition.Y) == 1
+            ? context.Rover.currentPosition.Y
+            : spaceShipLocation.Y > context.Rover.currentPosition.Y
+                ? context.Rover.currentPosition.Y + 1
+                : context.Rover.currentPosition.Y - 1;
+
+        var x = Math.Abs(spaceShipLocation.X - context.Rover.currentPosition.X) == 1
+            ? context.Rover.currentPosition.X
+            : spaceShipLocation.X > context.Rover.currentPosition.X
+            ? context.Rover.currentPosition.X + 1
+            : context.Rover.currentPosition.X - 1;
+
+        // needs to check if cell is available
+        
+        // should stop movement if reaches spaceship
+
+        return new Coordinate(x, y);
     }
 }
