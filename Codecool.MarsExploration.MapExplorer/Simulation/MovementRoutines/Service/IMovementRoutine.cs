923de0706b46e9974d9fde05071e2a61f0fca2d9
Gergely Papp <gregpappdev.gmail.com>
2023-03-07 22:23:10 +0100

    creates Movement routine interface

---
+++ Codecool.MarsExploration.MapExplorer/Simulation/MovementRoutines/Service/IMovementRoutine.cs
@@ -0,0 +1,9 @@
+﻿using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Simulation.MovementRoutines.Service;
+
+public interface IMovementRoutine
+{
+    public Coordinate NextStep(Coordinate currentPosition, Map map);
+}    
