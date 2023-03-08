a785b3a4a0483f93330168a18e6206d792651c67
Gergely Papp <gregpappdev.gmail.com>
2023-03-08 20:03:24 +0100

    creates primary constructor in MarsRover.cs

--- Codecool.MarsExploration.MapExplorer/MarsRover/MarsRover.cs
+++ Codecool.MarsExploration.MapExplorer/MarsRover/MarsRover.cs
@@ -2,7 +2,7 @@ using Codecool.MarsExploration.MapGenerator.Calculators.Model;
 
 namespace Codecool.MarsExploration.MapExplorer.MarsRover;
 
-public record MarsRover
+public record MarsRover(string Rover, Coordinate InitialPosition, int Sight, object O)
 {
     private string Id { get; }
     public Coordinate? CurrentPosition { get; set; }
