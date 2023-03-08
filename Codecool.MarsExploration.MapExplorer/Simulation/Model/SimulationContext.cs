0a929fa58f20a5bc13ae433aecd5054c69f265c7
esztivagyok <esztivoltam@gmail.com>
2023-03-08 14:13:06 +0100

    Add fixes to SimulationContext

--- Codecool.MarsExploration.MapExplorer/Simulation/Model/SimulationContext.cs
+++ Codecool.MarsExploration.MapExplorer/Simulation/Model/SimulationContext.cs
@@ -1,13 +1,12 @@
 using Codecool.MarsExploration.MapExplorer.Exploration;
-using Codecool.MarsExploration.MapExplorer.MarsRover.Model;
 using Codecool.MarsExploration.MapGenerator.Calculators.Model;
 using Codecool.MarsExploration.MapGenerator.MapElements.Model;
 
 namespace Codecool.MarsExploration.MapExplorer.Simulation.Model;
 
 public record SimulationContext 
-    (int Steps, int StepsToTimeout, MarsRoverModel Rover, 
-        Coordinate SpaceShipLocation, Map Map, IEnumerable<string> MonitorResources, ExplorationOutcome? Outcome)
+    (int Steps, int StepsToTimeout, MarsRover.MarsRover Rover, 
+        Coordinate SpaceShipLocation, Map Map, IEnumerable<string> MonitoredResources, ExplorationOutcome? Outcome)
 {
     public int Steps { get; set; } = Steps;
     public int StepsToTimeout { get; set; } = StepsToTimeout;
--- Codecool.MarsExploration.MapExplorer/Simulation/Service/ExplorationSimulator.cs
+++ Codecool.MarsExploration.MapExplorer/Simulation/Service/ExplorationSimulator.cs
@@ -1,12 +1,33 @@
+using System.Security.Cryptography.X509Certificates;
+using Codecool.MarsExploration.MapExplorer.Configuration;
+using Codecool.MarsExploration.MapExplorer.MapLoader;
 using Codecool.MarsExploration.MapExplorer.Simulation.Model;
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
 using Codecool.MarsExploration.MapGenerator.MapElements.Model;
 
 namespace Codecool.MarsExploration.MapExplorer.Simulation.Service;
 
-public class ExplorationSimulator : IExplorationSimulator
+public class ExplorationSimulator
 {
-    public SimulationContext ExploringSimulator(Map map, Configuration.Configuration configuration)
+    private Configuration.Configuration _configuration;
+    private MapLoader.MapLoader _mapLoader;
+    private MarsRover.MarsRover _rover;
+    private string _mapFile;
+
+   public ExplorationSimulator(MarsRover.MarsRover rover, string mapFile, Configuration.Configuration)
     {
-        return null;
+        _rover = rover;
+        _mapFile = mapFile;
+    }
+
+    public SimulationContext CreateSimulationContext(Map map, Configuration.Configuration configuration)
+    {
+        
+        return new SimulationContext(0, configuration.Timeout, _rover, configuration.LandingCoordinates, map, configuration.MonitoredResources, null);
+    }
+    public SimulationContext ExploringSimulator(Map map, Configuration.Configuration configuration, SimulationContext simulationContext)
+    {
+        simulationContext = new SimulationContext(0, configuration.Timeout, _rover, configuration.LandingCoordinates, map, configuration.MonitoredResources, null)
+        return simulationContext;
     }
 }
