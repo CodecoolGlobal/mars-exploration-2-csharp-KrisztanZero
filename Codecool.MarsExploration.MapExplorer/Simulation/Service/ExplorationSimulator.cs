285692fe11b1778fa7ac363f1ebef2a9768908eb
esztivagyok <esztivoltam@gmail.com>
2023-03-08 09:28:32 +0100

    Add simple logic to Simulation

--- Codecool.MarsExploration.MapExplorer/Simulation/Service/ExplorationSimulator.cs
+++ Codecool.MarsExploration.MapExplorer/Simulation/Service/ExplorationSimulator.cs
@@ -1,3 +1,5 @@
+using Codecool.MarsExploration.MapExplorer.Configuration;
+using Codecool.MarsExploration.MapExplorer.MapLoader;
 using Codecool.MarsExploration.MapExplorer.Simulation.Model;
 using Codecool.MarsExploration.MapGenerator.MapElements.Model;
 
@@ -5,6 +7,18 @@ namespace Codecool.MarsExploration.MapExplorer.Simulation.Service;
 
 public class ExplorationSimulator : IExplorationSimulator
 {
+
+    private IRoverDeployer _roverDeployer;
+    private IMapLoader _mapLoader;
+    private IConfigurationValidator _configurationValidator;
+
+    public ExplorationSimulator(IRoverDeployer roverDeployer, IMapLoader mapLoader,
+        IConfigurationValidator configurationValidator)
+    {
+        _roverDeployer = roverDeployer;
+        _mapLoader = mapLoader;
+        _configurationValidator = configurationValidator;
+    }
     public SimulationContext ExploringSimulator(Map map, Configuration.Configuration configuration)
     {
         
