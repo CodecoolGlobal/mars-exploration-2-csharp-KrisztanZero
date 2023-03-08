faaed762ca7806110d1f502acebfd40517386f06
esztivagyok <esztivoltam@gmail.com>
2023-03-08 14:43:13 +0100

    Add more items to ExplorationOutcome

--- Codecool.MarsExploration.MapExplorer/Simulation/Service/ExplorationSimulator.cs
+++ Codecool.MarsExploration.MapExplorer/Simulation/Service/ExplorationSimulator.cs
@@ -1,5 +1,6 @@
 using System.Security.Cryptography.X509Certificates;
 using Codecool.MarsExploration.MapExplorer.Configuration;
+using Codecool.MarsExploration.MapExplorer.Exploration;
 using Codecool.MarsExploration.MapExplorer.MapLoader;
 using Codecool.MarsExploration.MapExplorer.Simulation.Model;
 using Codecool.MarsExploration.MapGenerator.Calculators.Model;
@@ -10,16 +11,21 @@ namespace Codecool.MarsExploration.MapExplorer.Simulation.Service;
 public class ExplorationSimulator
 {
     private SimulationContext _simulationContext;
-
+    private ExplorationSimulationSteps.Service.ExplorationSimulationSteps _explorationSimulationSteps;
+    
    public ExplorationSimulator(MarsRover.MarsRover rover, Map map, Configuration.Configuration configuration)
    {
        _simulationContext = new SimulationContext(0, configuration.Timeout, rover, configuration.LandingCoordinates,
-           map, configuration.MonitoredResources, null);
+           map, configuration.MonitoredResources, ExplorationOutcome.Step);
    }
 
     public void Run ()
     {
-
+        while (_simulationContext.Outcome == ExplorationOutcome.Step)
+        {
+            _explorationSimulationSteps.Run(_simulationContext);
+        }
+        //while-on kívül Logot létrehozni
     }
 
 }
