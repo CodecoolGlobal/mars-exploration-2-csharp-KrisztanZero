1d41f6712d8d789ea6ab20ad1a8d3227e7a43cf5
akosnandornagy <109482995+akosnandornagy@users.noreply.github.com>
2023-03-08 10:07:57 +0100

    Update MarsRover.cs

--- Codecool.MarsExploration.MapExplorer/MarsRover/MarsRover.cs
+++ Codecool.MarsExploration.MapExplorer/MarsRover/MarsRover.cs
@@ -2,4 +2,4 @@ using Codecool.MarsExploration.MapGenerator.Calculators.Model;
 
 namespace Codecool.MarsExploration.MapExplorer.MarsRover;
 
-public record MarsRover(string Id, Coordinate? CurrentPosition, int Sight, IEnumerable<IEnumerable<Coordinate>> MonitoredResourcesPositions);
+public record MarsRover(string Id, Coordinate? CurrentPosition, int Sight, Dictionary<Coordinate, string>? MonitoredResources);
