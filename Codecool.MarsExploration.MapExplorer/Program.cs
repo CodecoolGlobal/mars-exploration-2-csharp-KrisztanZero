59e83b3580f705cbf157a489a715e92b0156512f
Gergely Papp <gregpappdev.gmail.com>
2023-03-08 11:59:26 +0100

    implements configuration of map in Program.cs

--- Codecool.MarsExploration.MapExplorer/Program.cs
+++ Codecool.MarsExploration.MapExplorer/Program.cs
@@ -1,4 +1,7 @@
-﻿using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+﻿using Codecool.MarsExploration.MapExplorer.Configuration;
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.Calculators.Service;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
 
 namespace Codecool.MarsExploration.MapExplorer;
 
@@ -10,6 +13,34 @@ class Program
     {
         string mapFile = $@"{WorkDir}\Resources\exploration-0.map";
         Coordinate landingSpot = new Coordinate(6, 6);
+        List<string> monitoredResources = new List<string>() { "*", "%" };
+        ICoordinateCalculator coordinateCalculator = new CoordinateCalculator();
+        MapLoader.MapLoader mapLoader = new MapLoader.MapLoader();
+
+        Configuration.Configuration configuration =
+            new Configuration.Configuration(mapFile, landingSpot, monitoredResources, 100);
+
+        ConfigurationValidator configurationValidator = new ConfigurationValidator();
+
+        Map map;
+        
+        if (configurationValidator.Validate(configuration, mapLoader, coordinateCalculator))
+        {
+            map = mapLoader.Load(mapFile);
+            
+            
+            
+            
+        }
+        else
+        {
+            Console.WriteLine("Configuration validation unsuccessful");
+        }
+        
+        
+
+
+
 
     }
 }
