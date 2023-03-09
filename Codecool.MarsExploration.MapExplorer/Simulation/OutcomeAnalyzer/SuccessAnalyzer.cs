a48d262418836cd20c548b6ba3055df26d80f2a8
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-03-09 18:35:21 +0100

    corrects SuccessAnalyzer class

--- Codecool.MarsExploration.MapExplorer/Simulation/OutcomeAnalyzer/SuccessAnalyzer.cs
+++ Codecool.MarsExploration.MapExplorer/Simulation/OutcomeAnalyzer/SuccessAnalyzer.cs
@@ -15,17 +15,15 @@ public class SuccessAnalyzer : IAnalyzer
         var minerals = scannedPositions.Count(c => c.Item2 == "*");
         var waters = scannedPositions.Count(c => c.Item2 == "%");
 
-        // Krisztián, please revise below recommendation
-        
-        /*var waterNearby = scannedPositions.Any(c =>
+        var waterNearby = scannedPositions.Any(c =>
             c.Item2 == "%" &&
-            scannedPositions.Any(nc => DistanceBetween(nc.Item1, c.Item1) <= 5));*/
+            scannedPositions.Any(nc => DistanceBetween(nc.Item1, c.Item1) <= 5));
         
-        /*var landingLocation = context.SpaceShipLocation;
-        var landingNearbyWater = coordinates.Any(c =>
-            scannedPositions[c] == "%" && DistanceBetween(landingLocation, c) <= 10);*/
+        var landingLocation = context.SpaceShipLocation;
+        var landingNearbyWater = scannedPositions.Any(c =>
+            c.Item2 == "%" && DistanceBetween(landingLocation, c.Item1) <= 10);
 
-        return minerals >= 10 || waters >= 10 /*|| waterNearby || landingNearbyWater*/;
+        return minerals >= 10 || waters >= 10 || waterNearby || landingNearbyWater;
     }
 
     private static int DistanceBetween(Coordinate from, Coordinate to)
